9b3c463ca635d642a3b6d64a6a2e84e672600932
lindsaycarlin <lcarlin@fwparker.org>
2019-03-01 08:32:50 -0600

    this is my searching problem set!

--- Searching/search_problems.py
+++ Searching/search_problems.py
@@ -7,15 +7,64 @@ from class and from Ch15 of the textbook website
 # word in the provided dictionary.  If there are more
 # than one longest word, print them all.
 
+char_list = []
+with open('dictionary.txt') as f:
+    word_list = [x.strip() for x in f]
+
+for word in word_list:
+    char_list.append(len(word))
+max_letters = max(char_list)
+print("The longest word in the dictionary has", max_letters, "letters.")
+for i in word_list:
+    if len(i) == 28:
+        print("The longest word in the dictionary is", i)
+
+
 #2.  (8pts)  Write code which finds
 # The total word count AND average word length
 # in "AliceInWonderLand.txt"
+
+import re
+def split_line(line):
+    return re.findall('[A-Za-z]+(?:\'[A-Za-z]+)?', line)
+
+
+alice_in_wonderland = open("AliceInWonderLand.txt")
+
+word_list = []
+for word in alice_in_wonderland:
+    words = split_line(word.strip())
+    for word in words:
+        print(word)
+        word_list.append(word)
+lens_words = []
+for word in word_list:
+    lens_words.append(len(word))
+print(word_list)
+
+print("There are", len(word_list), "words in 'Alice in Wonderland'")
+print("The average word in 'Alice in Wonderland is", sum(lens_words)/len(lens_words), "letters long.")
 
 # CHOOSE ONE OF THE FOLLOWING TWO PROBLEMS
 
 #3 (12pts)  How many times does "Cheshire" occur in"AliceInWonderLand.txt"?
 # How many times does "Cat" occur?
 # How many times does "Cheshire" immediately followed by "Cat" occur?
+
+cheshire_counter = 0
+cat_counter = 0
+cheshire_cat_counter = 0
+for i in range(len(word_list)):
+    if word_list[i].upper() == "CHESHIRE":
+        cheshire_counter += 1
+    if word_list[i].upper() == "CAT":
+        cat_counter += 1
+    if word_list[i].upper() == "CHESHIRE" and word_list[i + 1].upper() == "CAT":
+        cheshire_cat_counter += 1
+
+print(cheshire_counter)
+print(cat_counter)
+print(cheshire_cat_counter)
 
 #### OR #####
 
--- challenge_problems.py
+++ challenge_problems.py
@@ -1,9 +1,24 @@
+import random
+
 # PROBLEM 1 (Fibonacci)
 ## The Fibonacci sequence is a sequence of numbers that starts with 1, followed by 1 again.
 # Every next number is the sum of the two previous numbers.
 # I.e., the sequence starts with 1, 1, 2, 3, 5, 8, 13, 21,...
 # Write a program that calculates and prints the Fibonacci sequence
 # until the numbers get higher than 1000.
+
+fibonacci_numbers = []
+fibonacci = 0
+fibonacci_minus_two = 1
+fibonacci_minus_one = 1
+
+while fibonacci < 1000:
+    fibonacci = fibonacci_minus_one + fibonacci_minus_two
+    fibonacci_numbers.append(fibonacci)
+    fibonacci_minus_two = fibonacci_minus_one
+    fibonacci_minus_one = fibonacci
+
+print(fibonacci_numbers)
 
 # PROBLEM 2 (Dice Sequence)
 # You roll five six-sided dice, one by one.
@@ -13,9 +28,40 @@
 # but “1, 1, 4, 3, 6” is not. Determine the
 # probability of success using a simulation of a large number of trials.
 
+'''
+dice_rolls = []
+success = 0
+failure = 0
+for i in range(100):
+    dice_roll = []
+    for j in range(5):
+        die = random.randrange(1, 7)
+        dice_roll.append(die)
+    dice_rolls.append(dice_roll)
+    for k in range(len(dice_rolls)):
+        for l in range(len(dice_rolls[k])):
+            if dice_roll[k] > dice_roll[k-1]:
+                success += 1
+            else:
+                failure += 1
+print(success/(success+failure))
+
+'''
+
+
+
 # PROBLEM 3 (Number Puzzler)
 # A, B, C, and D are all different digits.
 # The number DCBA is equal to 4 times the number ABCD.
 # What are the digits?
 # Note: to make ABCD and DCBA conventional numbers, neither A nor D can be zero.
-# Use a quadruple-nested loop to solve. 
+# Use a quadruple-nested loop to solve.
+
+
+'''
+for i in range(10):
+    for j in range(10):
+        for k in range(10):
+            for l in range(10):
+                print("abcd")
+'''
