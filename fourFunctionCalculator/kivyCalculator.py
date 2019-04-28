e5f1e856904d9c0e686696ee6b6c8eca58daae1a
lindsaycarlin <lcarlin@fwparker.org>
2019-04-28 13:28:48 -0500

    four function calculator, made using kivy, meant to replicate the MacOS calculator as closely as possible (the input text is black and left-aligned as opposed to white and right-aligned)

--- fourFunctionCalculator/kivyCalculator.py
+++ fourFunctionCalculator/kivyCalculator.py
@@ -22,3 +22,22 @@
 # Match the font and text color as closely as you can.
 # Match the size of the app and the relative button sizes as closely as you can.
 # Match the button layout as closely as you can.
+
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.core.window import Window
+
+Window.size = (230, 320)
+
+class CalculatorApp(App):
+    def build(self):
+        return CalculatorLayout()
+
+class CalculatorLayout(BoxLayout):
+    def calculate(self, equation):
+        answer = eval(equation)
+        self.display.text = str(answer)
+
+if __name__ == "__main__":
+    app = CalculatorApp()
+    app.run()
